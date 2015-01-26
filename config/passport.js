8e5848ed0a57adb4a1e573174866cbf897913682
Alex Rock <hi@lxrck.com>
2015-01-26 17:12:42 -0600

    Removed unused Passport Auths

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,6 @@
 // load all the things we need
 var LocalStrategy    = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-var TwitterStrategy  = require('passport-twitter').Strategy;
-var InstagramStrategy = require('passport-instagram').Strategy;
+// var InstagramStrategy = require('passport-instagram').Strategy;
 
 // load up the user model
 var User       = require('../models/user');
@@ -131,233 +129,81 @@ module.exports = function(passport) {
     }));
 
     // =========================================================================
-    // FACEBOOK ================================================================
-    // =========================================================================
-    passport.use(new FacebookStrategy({
-
-        clientID        : configAuth.facebookAuth.clientID,
-        clientSecret    : configAuth.facebookAuth.clientSecret,
-        callbackURL     : configAuth.facebookAuth.callbackURL,
-        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-
-    },
-    function(req, token, refreshToken, profile, done) {
-
-        // asynchronous
-        process.nextTick(function() {
-
-            // check if the user is already logged in
-            if (!req.user) {
-
-                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-                    if (err)
-                        return done(err);
-
-                    if (user) {
-
-                        // if there is a user id already but no token (user was linked at one point and then removed)
-                        if (!user.facebook.token) {
-                            user.facebook.token = token;
-                            user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                            user.facebook.email = profile.emails[0].value;
-
-                            user.save(function(err) {
-                                if (err)
-                                    throw err;
-                                return done(null, user);
-                            });
-                        }
-
-                        return done(null, user); // user found, return that user
-                    } else {
-                        // if there is no user, create them
-                        var newUser            = new User();
-
-                        newUser.facebook.id    = profile.id;
-                        newUser.facebook.token = token;
-                        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                        newUser.facebook.email = profile.emails[0].value;
-
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
-
-            } else {
-                // user already exists and is logged in, we have to link accounts
-                var user            = req.user; // pull the user out of the session
-
-                user.facebook.id    = profile.id;
-                user.facebook.token = token;
-                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                user.facebook.email = profile.emails[0].value;
-
-                user.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, user);
-                });
-
-            }
-        });
-
-    }));
-
-    // =========================================================================
-    // TWITTER =================================================================
-    // =========================================================================
-    passport.use(new TwitterStrategy({
-
-        consumerKey     : configAuth.twitterAuth.consumerKey,
-        consumerSecret  : configAuth.twitterAuth.consumerSecret,
-        callbackURL     : configAuth.twitterAuth.callbackURL,
-        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-
-    },
-    function(req, token, tokenSecret, profile, done) {
-
-        // asynchronous
-        process.nextTick(function() {
-
-            // check if the user is already logged in
-            if (!req.user) {
-
-                User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
-                    if (err)
-                        return done(err);
-
-                    if (user) {
-                        // if there is a user id already but no token (user was linked at one point and then removed)
-                        if (!user.twitter.token) {
-                            user.twitter.token       = token;
-                            user.twitter.username    = profile.username;
-                            user.twitter.displayName = profile.displayName;
-
-                            user.save(function(err) {
-                                if (err)
-                                    throw err;
-                                return done(null, user);
-                            });
-                        }
-
-                        return done(null, user); // user found, return that user
-                    } else {
-                        // if there is no user, create them
-                        var newUser                 = new User();
-
-                        newUser.twitter.id          = profile.id;
-                        newUser.twitter.token       = token;
-                        newUser.twitter.username    = profile.username;
-                        newUser.twitter.displayName = profile.displayName;
-
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
-
-            } else {
-                // user already exists and is logged in, we have to link accounts
-                var user                 = req.user; // pull the user out of the session
-
-                user.twitter.id          = profile.id;
-                user.twitter.token       = token;
-                user.twitter.username    = profile.username;
-                user.twitter.displayName = profile.displayName;
-
-                user.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, user);
-                });
-            }
-
-        });
-
-    }));
-
-
-    // =========================================================================
     // INSTAGRAM ===============================================================
     // =========================================================================
-    passport.use(new InstagramStrategy({
+    // passport.use(new InstagramStrategy({
 
-        clientID        : configAuth.instagramAuth.clientID,
-        clientSecret    : configAuth.instagramAuth.clientSecret,
-        callbackURL     : configAuth.instagramAuth.callbackURL,
-        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+    //     clientID        : configAuth.instagramAuth.clientID,
+    //     clientSecret    : configAuth.instagramAuth.clientSecret,
+    //     callbackURL     : configAuth.instagramAuth.callbackURL,
+    //     passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
 
-    },
-    function(req, token, refreshToken, profile, done) {
+    // },
+    // function(req, token, refreshToken, profile, done) {
 
-        // asynchronous
-        process.nextTick(function() {
+    //     // asynchronous
+    //     process.nextTick(function() {
 
-            // check if the user is already logged in
-            if (!req.user) {
+    //         // check if the user is already logged in
+    //         if (!req.user) {
 
-                User.findOne({ 'instagram.id' : profile.id }, function(err, user) {
-                    if (err)
-                        return done(err);
+    //             User.findOne({ 'instagram.id' : profile.id }, function(err, user) {
+    //                 if (err)
+    //                     return done(err);
 
-                    if (user) {
+    //                 if (user) {
 
-                        // if there is a user id already but no token (user was linked at one point and then removed)
-                        if (!user.instagram.token) {
-                            user.instagram.token = token;
-                            user.instagram.name  = profile.username;
-                            user.instagram.displayName  = profile.displayName;
-                            // user.instagram.email = profile.emails[0].value;
+    //                     // if there is a user id already but no token (user was linked at one point and then removed)
+    //                     if (!user.instagram.token) {
+    //                         user.instagram.token = token;
+    //                         user.instagram.name  = profile.username;
+    //                         user.instagram.displayName  = profile.displayName;
+    //                         // user.instagram.email = profile.emails[0].value;
 
-                            user.save(function(err) {
-                                if (err)
-                                    throw err;
-                                return done(null, user);
-                            });
-                        }
+    //                         user.save(function(err) {
+    //                             if (err)
+    //                                 throw err;
+    //                             return done(null, user);
+    //                         });
+    //                     }
 
-                        return done(null, user); // user found, return that user
-                    } else {
-                        // if there is no user, create them
-                        var newUser            = new User();
+    //                     return done(null, user); // user found, return that user
+    //                 } else {
+    //                     // if there is no user, create them
+    //                     var newUser            = new User();
 
-                        newUser.instagram.id    = profile.id;
-                        newUser.instagram.token = token;
-                        newUser.instagram.name  = profile.username;
-                        newUser.instagram.displayName  = profile.displayName;
+    //                     newUser.instagram.id    = profile.id;
+    //                     newUser.instagram.token = token;
+    //                     newUser.instagram.name  = profile.username;
+    //                     newUser.instagram.displayName  = profile.displayName;
 
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
+    //                     newUser.save(function(err) {
+    //                         if (err)
+    //                             throw err;
+    //                         return done(null, newUser);
+    //                     });
+    //                 }
+    //             });
 
-            } else {
-                // user already exists and is logged in, we have to link accounts
-                var user            = req.user; // pull the user out of the session
+    //         } else {
+    //             // user already exists and is logged in, we have to link accounts
+    //             var user            = req.user; // pull the user out of the session
 
-                user.instagram.id    = profile.id;
-                user.instagram.token = token;
-                user.instagram.name  = profile.username;
-                user.instagram.displayName  = profile.displayName;
+    //             user.instagram.id    = profile.id;
+    //             user.instagram.token = token;
+    //             user.instagram.name  = profile.username;
+    //             user.instagram.displayName  = profile.displayName;
 
-                user.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, user);
-                });
+    //             user.save(function(err) {
+    //                 if (err)
+    //                     throw err;
+    //                 return done(null, user);
+    //             });
 
-            }
-        });
+    //         }
+    //     });
 
-    }));
+    // }));
 
 };
 
