3d4c3229470d00b47592da4f743b04380036809d
Alex Rock <hi@lxrck.com>
2015-01-28 04:35:25 -0600

    Mongolab for heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-	 'url' : 'mongodb://127.0.0.1/maverick_mayhem',
+	 'url' : process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/maverick_mayhem',
 	 'test' : 'mongodb://localhost/maverick_mayhem_test'
 };
