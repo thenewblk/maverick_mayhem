4bc9fa0d1d3a330c2504e48f6f63ede493623f7f
bangarang <me@alexjrock.com>
2015-02-13 14:01:22 -0600

    Styling

--- public/js/all.js
+++ public/js/all.js
@@ -10305,4 +10305,20 @@ $(function () {
   // $video.on('canplaythrough', showVideo);
   checkReadyState();
 
+
+  $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').stop().animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          $('.navigation').removeClass('active');
+          $('.navigation-items').removeClass('active');
+          return false;
+        }
+      }
+  });
+
 });
